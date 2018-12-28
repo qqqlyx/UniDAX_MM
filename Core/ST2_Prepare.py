74611aece0fd12603cb6e048ff8dcf70bd9baca2
RobinLiu <lyx.better@gmail.com>
2018-12-28 15:38:34 +0800

    up

--- Core/ST2_Prepare.py
+++ Core/ST2_Prepare.py
@@ -17,14 +17,14 @@ import shutil
 import os
 from Core import Tokens
 #显示所有列
-pd.set_option('display.max_columns', None)
-#显示所有行
-pd.set_option('display.max_rows', None)
-#调整显示宽度
-pd.set_option('display.height',1000)
-pd.set_option('display.max_rows',500)
-pd.set_option('display.max_columns',500)
-pd.set_option('display.width',1000)
+# pd.set_option('display.max_columns', None)
+# #显示所有行
+# pd.set_option('display.max_rows', None)
+# #调整显示宽度
+# pd.set_option('display.height',1000)
+# pd.set_option('display.max_rows',500)
+# pd.set_option('display.max_columns',500)
+# pd.set_option('display.width',1000)
 
 '''
 读取参数文件,遍历对应文件夹
--- Run/batWriter.py
+++ Run/batWriter.py
@@ -1,13 +1,23 @@
+from Core import Tokens
+
 stock_list = ['ethusdt', 'btcusdt', 'ltcusdt', 'etcusdt', 'ethbtc', 'ltcbtc',
               'yoousdt','zrxusdt','omgusdt','mcousdt','manausdt','sntusdt',
               'wtcusdt','gntusdt','repusdt','cmtusdt','aeusdt','elfusdt','pptusdt']
 
 
 for code in stock_list:
-    path = 'run_' + code + '.bat'
-    f = open(path,'w')
+    path = 'MM\\run_' + code + '.bat'
+    f = open(path, 'w')
 
     f.write('python D:\\Robin\\UniDAX_MM\\Monitor\\MM_Monitor.py ' + code)
     f.write('\npause')
     f.close
 
+for code in stock_list:
+    path = Tokens._path + '\\Run\\ST\\run_ST_' + code + '.bat'
+    f = open(path, 'w')
+
+    path = 'Python ' + Tokens._path + '\\Monitor\\SelfTrading_Monitor.py '
+    f.write(path + code)
+    f.write('\npause')
+    f.close
